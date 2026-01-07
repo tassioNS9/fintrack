f2468a34872ab8b0a47ba201b669f6b22be8696f
Tassio Neves <tassions@hotmail.com>
2026-01-07 16:28:01 -0300

    chore: add shadcn setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-import js from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import react from 'eslint-plugin-react' 
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
-import globals from 'globals'
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 
 export default [
   { ignores: ['dist'] },
@@ -34,11 +34,12 @@ export default [
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
       'react/jsx-no-target-blank': 'off',
+      'react/prop-types': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
-  eslintConfigPrettier
-]
+  eslintConfigPrettier,
+];
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Button } from './components/ui/button';
+
 const App = () => {
-  return <div className="flex px-4 text-red-600">Hello Wgggggggorld</div>;
+  return (
+    <div className="flex flex-col px-4 text-red-600">
+      Hello Wgggggggorld
+      <Button className="w-[200px]">Hello World</Button>
+    </div>
+  );
 };
 
 export default App;
