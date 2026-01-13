d05646380a67fb0d96b7c2b8d64d09676810b096
Tassio Neves <tassions@hotmail.com>
2026-01-13 16:09:18 -0300

    feat: include user ID in balance query to prevent cache data issues

--- src/api/hooks/user.js
+++ src/api/hooks/user.js
@@ -1,7 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
+import { useContext } from 'react';
 
+import { AuthContext } from '@/contexts/auth';
 import { UserService } from '@/services/users';
-
 export const getUserBalanceQueryKey = ({ userId, from, to }) => {
   if (!from || !to) {
     return ['balance'];
@@ -10,8 +11,10 @@ export const getUserBalanceQueryKey = ({ userId, from, to }) => {
 };
 
 export const useGetUserBalance = () => {
+  // Temos que passar o id do usuário na query, pois se não outro usuário a ser logado vai com dados do cache dessa requisição
+  const { user } = useContext(AuthContext);
   return useQuery({
-    queryKey: ['getBalance'],
+    queryKey: ['getBalance', user?.id],
     queryFn: () => {
       return UserService.getBalance();
     },
