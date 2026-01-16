bfcd2fa3d1085588f2bc0aaac92c8924eee22a59
Tassio Neves <tassions@hotmail.com>
2026-01-16 13:09:07 -0300

    feat: ensure transaction date and type are rendered properly on table

--- src/components/transactions-table.jsx
+++ src/components/transactions-table.jsx
@@ -1,7 +1,11 @@
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR';
 import { useSearchParams } from 'react-router';
 
 import { useGetTransactions } from '@/api/hooks/transaction';
+import { formatCurrency } from '@/helpers/currency';
 
+import TransactionTypeBadge from './transaction-type-badge';
 import { DataTable } from './ui/data-table';
 
 const columns = [
@@ -14,14 +18,26 @@ const columns = [
   {
     accessorKey: 'type',
     header: 'Tipo',
+    cell: ({ row: { original: transaction } }) => {
+      return <TransactionTypeBadge variant={transaction.type.toLowerCase()} />;
+    },
   },
   {
     accessorKey: 'date',
     header: 'Data',
+    // Temos uma propriedade cell que permite customizar o conteúdo da célula
+    cell: ({ row: { original: transaction } }) => {
+      return format(new Date(transaction.date), "dd 'de' MMMM 'de' yyyy", {
+        locale: ptBR,
+      });
+    },
   },
   {
     accessorKey: 'amount',
     header: 'Valor',
+    cell: ({ row: { original: transaction } }) => {
+      return formatCurrency(transaction.amount);
+    },
   },
   {
     accessorKey: 'actions',
